033ffc25b3220d7e84ae8701f1d1c075ea3b8630
Max Stern <stern.maxwell@gmail.com>
2019-08-18 12:50:02 -0700

    server.js

---
+++ src/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log('server is running on port: ', port);
+});
